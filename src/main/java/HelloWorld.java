c536f7e8a91b713d43b045ba23ff019c4db23dc0
Max Fr <fruehaufmaximilian@gmail.com>
2017-10-24 09:28:46 +0200

    this is the first commit in this tree

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld extends MiniJava {
+
+	public static void main(String[] args) {
+		write("Hello World \n" +
+				"this is a heart: ♥\n" +
+				"this is a penguin: \uD83D\uDC27\n");
+
+	}
+}
---
+++ src/main/java/MiniJava.java
@@ -0,0 +1,94 @@
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class MiniJava {
+
+  public static String readString(String text) {
+    JFrame frame = new JFrame();
+    String s = JOptionPane.showInputDialog(frame, text);
+    frame.dispose();
+
+    if (s == null)
+      System.exit(0);
+    return s;
+  }
+
+  public static String readString() {
+    return readString("Eingabe:");
+  }
+
+  public static int readInt(String text) {
+    JFrame frame = new JFrame();
+    String s = JOptionPane.showInputDialog(frame, text);
+    frame.dispose();
+
+    int x;
+    if (s == null)
+      System.exit(0);
+    try {
+      x = Integer.parseInt(s.trim());
+    } catch (NumberFormatException e) {
+      x = readInt(text);
+    }
+    return x;
+  }
+
+  public static int readInt() {
+    return readInt("Geben Sie eine ganze Zahl ein:");
+  }
+
+  public static int read(String text) {
+    return readInt(text);
+  }
+
+  public static int read() {
+    return readInt();
+  }
+
+  public static double readDouble(String text) {
+    JFrame frame = new JFrame();
+    String s = JOptionPane.showInputDialog(frame, text);
+    frame.dispose();
+
+    double x;
+    if (s == null)
+      System.exit(0);
+    try {
+      x = Double.parseDouble(s.trim());
+    } catch (NumberFormatException e) {
+      x = readDouble(text);
+    }
+    return x;
+  }
+
+  public static double readDouble() {
+    return readDouble("Geben Sie eine Zahl ein:");
+  }
+
+  public static void write(String output) {
+    JFrame frame = new JFrame();
+    JOptionPane.showMessageDialog(frame, output, "Ausgabe", JOptionPane.PLAIN_MESSAGE);
+    frame.dispose();
+  }
+
+  public static void write(int output) {
+    write("" + output);
+  }
+
+  public static void write(double output) {
+    write("" + output);
+  }
+
+  public static int randomInt(int minval, int maxval) {
+    return minval + (new java.util.Random()).nextInt(maxval - minval + 1);
+  }
+
+  public static int drawCard() {
+    return randomInt(2, 11);
+  }
+
+  public static int dice() {
+    return randomInt(1, 6);
+  }
+}
