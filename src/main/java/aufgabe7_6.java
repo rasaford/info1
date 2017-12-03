f8726b03f245bcdcf8ae7192b50b616673e7fe77
Max Fr <fruehaufmaximilian@gmail.com>
2017-12-03 16:26:32 +0100

    solve task 6 on blatt7

--- src/main/java/aufgabe7_6.java
+++ src/main/java/aufgabe7_6.java
@@ -1,29 +1,38 @@
+import java.util.Arrays;
+
 public class aufgabe7_6 {
 
   // utf8: "Köpfchen in das Wasser, Schwänzchen in die Höh." -CIA-Verhörmethode
   public static void main(String[] args) {
+    int[] array = new int[]{2, 1, -44, 55};
+    slowSort(array);
+    System.out.println(Arrays.toString(array));
 
+    int[] array2 = new int[]{2, 1, -44, 55};
+    evenSlowerSort(array2);
+    System.out.println(Arrays.toString(array2));
   }
 
   public static void slowSort(int[] array) {
-    slowSortR(array, 0, array.length);
+    slowSortR(array, 0, array.length - 1);
   }
 
   private static void slowSortR(int[] array, int min, int max) {
-    if (max - min == 1) {
+    if (max - min < 2) {
       if (array[max] < array[min]) {
         swap(array, min, max);
       }
       return;
     }
-    slowSortR(array, min, max / 2);
-    slowSortR(array, max / 2, max);
-    int index = max / 2;
-    if (array[max / 2] < array[max - 1]) {
-      index = max - 1;
+    int mid = (max - min) / 2;
+    slowSortR(array, min, min + mid);
+    slowSortR(array, min + mid + 1, max);
+    int partialMax = min + mid;
+    if (array[partialMax] < array[max]) {
+      partialMax = max;
     }
-    swap(array, index, max - 1);
-    slowSortR(array, min, max - 2);
+    swap(array, partialMax, max);
+    slowSortR(array, min, max - 1);
   }
 
   private static void swap(int[] array, int a, int b) {
@@ -32,15 +41,29 @@ public class aufgabe7_6 {
     array[b] = temp;
   }
 
-  private static int[] partition(int[] array, int min, int max) {
-    int[] out = new int[max - min];
-    for (int i = min; i < max; i++) {
-      out[i - min] = array[i];
-    }
-    return out;
+  public static void evenSlowerSort(int[] array) {
+    evenSlowerSortR(array, 0, array.length - 1);
   }
 
-  private static void evenSlowerSort(int[] array) {
-
+  private static void evenSlowerSortR(int[] array, int min, int max) {
+    if (max - min < 2) {
+      if (array[max] < array[min]) {
+        swap(array, min, max);
+      }
+      return;
+    }
+    int mid = (max - min) / 3;
+    evenSlowerSortR(array, min, min + mid);
+    evenSlowerSortR(array, min + mid + 1, min + 2 * mid);
+    evenSlowerSortR(array, min + 2 * mid + 1, max);
+    int partialMax = min + mid;
+    if (array[partialMax] < array[min + 2 * mid]) {
+      partialMax = min + 2 * mid;
+    }
+    if (array[partialMax] < array[max]) {
+      partialMax = max;
+    }
+    swap(array, partialMax, max);
+    evenSlowerSortR(array, min, max - 1);
   }
 }
--- src/test/java/TestAufgabe7_6.java
+++ src/test/java/TestAufgabe7_6.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
 import org.junit.Test;
 
 public class TestAufgabe7_6 {
@@ -8,8 +9,85 @@ public class TestAufgabe7_6 {
 
   @Test
   public void slowSort() {
-    int[] array = new int[] {4,3,1,2,5};
+    int[] array = new int[]{4, 3, 1, 2, 5};
     aufgabe7_6.slowSort(array);
-   assertArrayEquals(new int[]{1, 2, 3, 4, 5}, array);
+    assertArrayEquals(new int[]{1, 2, 3, 4, 5}, array);
+  }
+
+  @Test
+  public void slowSort1() {
+    int[] array = new int[]{2, 1};
+    aufgabe7_6.slowSort(array);
+    assertArrayEquals(new int[]{1, 2}, array);
+  }
+
+  @Test
+  public void slowSort2() {
+    int[] array = new int[]{2, 1, -44, 55};
+    aufgabe7_6.slowSort(array);
+    assertArrayEquals(new int[]{-44, 1, 2, 55}, array);
+  }
+
+  @Test
+  public void slowSort3() {
+    int[] array = new int[]{0, 0, -11, 22, 123, 0};
+    aufgabe7_6.slowSort(array);
+    assertArrayEquals(new int[]{-11, 0, 0, 0, 22, 123}, array);
+  }
+
+  @Test
+  public void slowSort4() {
+    int[] array = new int[]{2, 4, 6, 8, 10, 12, 14, 16, 18};
+    aufgabe7_6.slowSort(array);
+    assertArrayEquals(new int[]{2, 4, 6, 8, 10, 12, 14, 16, 18}, array);
+  }
+
+  @Test
+  public void slowSort5() {
+    int[] array = new int[]{17, 15, 13, 11, 9, 7, 5, 3, 1};
+    aufgabe7_6.slowSort(array);
+    assertArrayEquals(new int[]{1, 3, 5, 7, 9, 11, 13, 15, 17}, array);
+  }
+
+  @Test
+  public void evenSlowerSort() {
+    int[] array = new int[]{4, 3, 1, 2, 5};
+    aufgabe7_6.evenSlowerSort(array);
+    assertArrayEquals(new int[]{1, 2, 3, 4, 5}, array);
+  }
+
+  @Test
+  public void evenSlowerSort1() {
+    int[] array = new int[]{2, 1};
+    aufgabe7_6.evenSlowerSort(array);
+    assertArrayEquals(new int[]{1, 2}, array);
+  }
+
+  @Test
+  public void evenSlowerSort2() {
+    int[] array = new int[]{2, 1, -44, 55};
+    aufgabe7_6.evenSlowerSort(array);
+    assertArrayEquals(new int[]{-44, 1, 2, 55}, array);
+  }
+
+  @Test
+  public void evenSlowerSort3() {
+    int[] array = new int[]{0, 0, -11, 22, 123, 0};
+    aufgabe7_6.evenSlowerSort(array);
+    assertArrayEquals(new int[]{-11, 0, 0, 0, 22, 123}, array);
+  }
+
+  @Test
+  public void evenSlowerSort4() {
+    int[] array = new int[]{2, 4, 6, 8, 10, 12, 14, 16, 18};
+    aufgabe7_6.evenSlowerSort(array);
+    assertArrayEquals(new int[]{2, 4, 6, 8, 10, 12, 14, 16, 18}, array);
+  }
+
+  @Test
+  public void evenSlowerSort5() {
+    int[] array = new int[]{17, 15, 13, 11, 9, 7, 5, 3, 1};
+    aufgabe7_6.evenSlowerSort(array);
+    assertArrayEquals(new int[]{1, 3, 5, 7, 9, 11, 13, 15, 17}, array);
   }
 }
